9feef4306c71b71497539711841ba6170fc879ed
ALIF FIRDAUS <aliffirdaus@petaniweb.com>
2024-10-10 18:43:24 +0700

    feat: next-auth integrated

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,37 +1,39 @@
 "use client";
 
-import { signIn, useSession } from "next-auth/react";
-import React from "react";
+import React, { useEffect } from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const Dashboard = () => {
 	const { data: session } = useSession();
+	const router = useRouter();
+
+	useEffect(() => {
+		if (session) {
+			router.push("/movies");
+		}
+	}, [session, router]);
 
 	return (
 		<>
-			{session ? (
-				<>
-					<main className="text-4xl text-black flex items-center justify-center">
-						Welcome back!
-					</main>
-				</>
+			{!session ? (
+				<main className="text-4xl text-black flex flex-col gap-10 items-center justify-center">
+					Log in
+					<button
+						onClick={() => signIn("google")}
+						className="p-6 bg-red-500 text-lg"
+					>
+						Sign in with Google
+					</button>
+					<button
+						onClick={() => signIn("github")}
+						className="p-6 bg-red-500 text-lg"
+					>
+						Sign in with Github
+					</button>
+				</main>
 			) : (
-				<>
-					<main className="text-4xl text-black flex flex-col gap-10 items-center justify-center">
-						Log in
-						<button
-							onClick={() => signIn("google")}
-							className="p-6 bg-red-500 text-lg"
-						>
-							Sign in with Google
-						</button>
-						<button
-							onClick={() => signIn("github")}
-							className="p-6 bg-red-500 text-lg"
-						>
-							Sign in with Github
-						</button>
-					</main>
-				</>
+				<></>
 			)}
 		</>
 	);
